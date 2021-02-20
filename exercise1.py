d398660543e0bbf02adcf2cac7af8f270f92dfb2
xrlie <c.aviles.rz@gmail.com>
2021-02-20 12:37:47 -0600

    FIX exercise 1

--- exercise1.py
+++ exercise1.py
@@ -18,15 +18,21 @@ No se puede utilizar el método length
 """
 
 def length_number (number):
-    if (number > 0):                                            # Verifico si es un número positivo.
-        length = 0                                              # Inicializo la variable que guarda el largo del número.
-        for i in range (100):                                   # Con un for hago una comparación iterativa para contar cuántos dígitos tiene el número.
-            aux = 10 ** i
-            a = number / aux
-            if a > 1:
-                length += 1
-            else:
-                break
+    if type(number) != int:                                 # Verifico si es un número positivo.
+        return 'No ingresaste un número entero'
+    length = 0                                              # Inicializo la variable que guarda el largo del número.
+    alert = ''
+    for i in range (100):                                   # Con un for hago una comparación iterativa para contar cuántos dígitos tiene el número.
+        aux = 10 ** i
+        a = number / aux
+        if a >= 1:
+            length += 1
+        elif a == 0:
+            alert = 'No se pueden ingresar números negativos'
+            break
+        else:
+            break
+    if alert == '':
         return f'El largo de {number} es', length               # De vuelvo el largo del número.
     else:
         return None, 'No se pueden utilizar números negativos'  # Se imprime un mensaje cuando el valor es negativo.
